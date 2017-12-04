618d33978d238c7d931098465ce72ad39d81d807
Advaita Krishna das <advaita.krishna.das@gmail.com>
2017-12-04 17:38:26 +0300

    long report

--- app/validation.py
+++ app/validation.py
@@ -8,9 +8,14 @@ def validate(html):
 
     if table:
         for ridx, row in enumerate(table):
+            columns_cnt = len(row)
+
+            if columns_cnt not in [5, 36]:
+                __error(result, "Row should contain 5 or 36 column", row=ridx, column=0)
+
             if not row[0]:
                 __error(result, "Name is not provided", row=ridx, column=0)
-            for i in range(1, 5):
+            for i in range(1, columns_cnt):
                 if not isinstance(row[i], int):
                     __error(result, "Not a numeric value {}".format(row[i]), row=ridx, column=i)
     else:
